e0fd32a9eba274af9e3472070e0853ca4f4fe38b
hasrul sani <hasrulsani@gmail.com>
2024-11-12 07:21:37 +0800

    Memabahkan list

--- index.php
+++ index.php
@@ -7,5 +7,11 @@
 </head>
 <body>
     Hello World
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut non explicabo eaque enim blanditiis aliquam, eius dolores. Soluta consectetur facilis harum ipsum reiciendis iure obcaecati nulla eos natus iste. Velit!</p>
+    <ul>
+        <li>Hasrul Sani
+            
+        </li>
+    </ul>
 </body>
 </html>
